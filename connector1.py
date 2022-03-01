8acd57c3f7066124cfc005383d629f9d29a68837
angrygoatman <51275695+angrygoatman@users.noreply.github.com>
2022-02-28 20:47:54 -0800

    Add files via upload

---
+++ connector1.py
@@ -0,0 +1,55 @@
+import boto3
+import pandas as pd
+import os
+
+
+s3 = boto3.resource(
+    service_name='s3',
+    region_name='us-east-1',
+    aws_access_key_id='AKIAQXAY6OL5ENNGVHVX',
+    aws_secret_access_key='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'
+)
+
+
+import os
+os.environ["AWS_DEFAULT_REGION"] = 'us-east-1'
+os.environ["AWS_ACCESS_KEY_ID"] = 'AKIAQXAY6OL5ENNGVHVX'
+os.environ["AWS_SECRET_ACCESS_KEY"] = '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'
+
+
+for bucket in s3.buckets.all():
+    print(bucket.name)
+
+    
+# Make dataframes
+foo = pd.DataFrame({'x': [1, 2, 3], 'y': ['a', 'b', 'c']})
+bar = pd.DataFrame({'x': [10, 20, 30], 'y': ['aa', 'bb', 'cc']})
+
+# Save to csv
+foo.to_csv('foo.csv')
+bar.to_csv('bar.csv')
+
+s3.Bucket('poobucket2357').upload_file(Filename='foo.csv', Key='foo.csv')
+s3.Bucket('poobucket2357').upload_file(Filename='bar.csv', Key='bar.csv')
+
+for obj in s3.Bucket('poobucket2357').objects.all():
+    print(obj)
+
+obj = s3.Bucket('poobucket2357').Object('foo.csv').get()
+foo = pd.read_csv(obj['Body'], index_col=0)
+
+print(foo)
+
+obj = s3.Bucket('poobucket2357').Object('bar.csv').get()
+bar = pd.read_csv(obj['Body'], index_col=0)
+
+print(bar)
+
+
+s3.Bucket('poobucket2357').download_file(Key='foo.csv', Filename='foo2.csv')
+x = pd.read_csv('foo2.csv', index_col=0)
+
+print(x)
+
+
+
